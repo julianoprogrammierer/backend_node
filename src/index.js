ae93ab3c6779642f74d8f0730945349acd085c04
julianoprogrammierer <julianoprogrammierer@gmail.com>
2020-10-01 16:52:12 -0300

    Revert "Update index.js"
    This reverts commit 3ed67d5194e78bcd907ae4769476e5f4cabd9a24.

--- src/index.js
+++ src/index.js
@@ -16,15 +16,11 @@ app.use(express.json());
 const projects = [];
 app.get('/projects', (request, response)=>{
     
-   const { title } = request.query;
-
-   const results = title
-   ? projects.filter(project => project.title.includes(title))
-   : projects;
-
-  
+   //const {title, owner } = request.query;
+   //console.log(title);
+   //console.log(owner);
    
-   return response.json(results);
+   return response.json(projects);
 });
 app.post('/projects', (request, response)=>{
     const { title, owner } = request.body;
@@ -34,53 +30,24 @@ app.post('/projects', (request, response)=>{
     return response.json(project);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
 app.put('/projects/:id', (request, response)=>{
-   const { id } = request.params;
-   const { title, owner } = request.body;
-
-   const projectIndex = projects.findIndex(project => project.id = id);
-
-   if (projectIndex < 0) {
-       return response.status(400).json({ error: 'project not found.'})
-   }
-
-   const project = {
-    id,
-    title, 
-    owner,
-    };
-
-    projects[projectIndex] = project;
-    return response.json(project);
+    const params = request.params;
+    console.log(params);
+    
+    return response.json([
+        'project 4',
+        'project 2',
+        'project 3',
+        
+    ]);
 });
-
-
 app.delete('/projects/:id', (request, response)=>{
-    const { id } = request.params;
-    
-    const projectIndex = projects.findIndex(project => project.id = id);
-
-   if (projectIndex < 0) {
-       return response.status(400).json({ error: 'project not found.'})
-   }
-
-   projects.splice(projectIndex, 1);
-
-    
-    return response.status(204).send();
+    return response.json([
+        
+        'project 2',
+        'project 3',
+       
+    ]);
 });
 
 
